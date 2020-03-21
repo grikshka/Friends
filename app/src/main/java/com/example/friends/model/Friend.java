8e3932795e9a7ff3123986e147f5792931b3e1ec
schemabuoi <charyszczakg@gmail.com>
2020-03-21 14:23:06 +0100

    Create Friend entity

---
+++ app/src/main/java/com/example/friends/model/Friend.java
@@ -0,0 +1,41 @@
+package com.example.friends.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Friend {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String name;
+    private String email;
+    private boolean favourite;
+
+    public Friend(String name, String email, boolean favourite) {
+        this.name = name;
+        this.email = email;
+        this.favourite = favourite;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+}
