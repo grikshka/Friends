23e1e96dca4ce2973c9b9aac1e2717ed5faaa7f6
schemabuoi <charyszczakg@gmail.com>
2020-03-21 19:15:31 +0100

    Implement populating database

--- app/src/main/java/com/example/friends/model/database/FriendsDatabase.java
+++ app/src/main/java/com/example/friends/model/database/FriendsDatabase.java
@@ -1,10 +1,13 @@
 package com.example.friends.model.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.friends.entity.Friend;
 
@@ -22,8 +25,46 @@ public abstract class FriendsDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     FriendsDatabase.class, "friends_database").
                     fallbackToDestructiveMigration().
+                    addCallback(populateCallback).
                     build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback populateCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+        }
+    };
+
+    private static class PopulateTask extends AsyncTask<Void, Void, Void> {
+
+        private FriendDao friendDao;
+
+        private PopulateTask(FriendsDatabase db)
+        {
+            friendDao = db.friendDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+            friendDao.insert(new Friend("Grzegorz Charyszczak", "schemabuoi@gmail.com", true));
+            friendDao.insert(new Friend("Oliwia Skrzypaszek", "oliwiaskrzypaszek@gmail.com", true));
+            friendDao.insert(new Friend("Marcin Dwornikowski", "mdwornikowski@gmail.com", true));
+            friendDao.insert(new Friend("Iga Molendowska", "igamolendowska@gmail.com", true));
+            friendDao.insert(new Friend("David Kalatzis", "davidkalatzis@gmail.com", true));
+            friendDao.insert(new Friend("Mate Kiss", "matekiss@gmail.com", false));
+            friendDao.insert(new Friend("Nedas Surkus", "nedassurkus@gmail.com", false));
+            friendDao.insert(new Friend("Marek Stancik", "marekstancik@gmail.com", false));
+            friendDao.insert(new Friend("Jan Toth", "jantoth@gmail.com", false));
+            friendDao.insert(new Friend("Alex Pedersen", "alexpedersen@gmail.com", false));
+            friendDao.insert(new Friend("Kuba Rewald", "kubarewald@gmail.com", false));
+            friendDao.insert(new Friend("Radoslaw Haller", "radoslawhaller@gmail.com", false));
+            friendDao.insert(new Friend("Adi Tutor", "aditutor@gmail.com", false));
+            return null;
+        }
     }
 }
