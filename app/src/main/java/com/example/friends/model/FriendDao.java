f544a6ad31f515f7d7181dbe89b4fa152311479b
schemabuoi <charyszczakg@gmail.com>
2020-03-21 14:50:17 +0100

    Create FriendDao.java

---
+++ app/src/main/java/com/example/friends/model/FriendDao.java
@@ -0,0 +1,26 @@
+package com.example.friends.model;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface FriendDao {
+
+    @Insert
+    void insert(Friend friend);
+
+    @Update
+    void update(Friend friend);
+
+    @Delete
+    void delete(Friend friend);
+
+    @Query("SELECT * FROM friend ORDER BY favourite, name")
+    LiveData<List<Friend>> getAll();
+}
