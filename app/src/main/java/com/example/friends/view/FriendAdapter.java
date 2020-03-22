e040a964551e5f18492f5c8ed076ad862c150a3c
schemabuoi <charyszczakg@gmail.com>
2020-03-22 15:33:35 +0100

    Create FriendAdapter.java

---
+++ app/src/main/java/com/example/friends/view/FriendAdapter.java
@@ -0,0 +1,82 @@
+package com.example.friends.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.friends.R;
+import com.example.friends.entity.Friend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.FriendHolder>
+{
+
+    private List<Friend> friendList;
+
+    @NonNull
+    @Override
+    public FriendHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.friend_item, parent, false);
+        return new FriendHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendHolder holder, int position) {
+        if(friendList != null)
+        {
+            Friend currentFriend = friendList.get(position);
+            holder.tvName.setText(currentFriend.getName());
+            if (currentFriend.isFavourite())
+            {
+                holder.imgFavourite.setImageResource(R.drawable.favourite_true);
+            }
+            else
+            {
+                holder.imgFavourite.setImageResource(R.drawable.favourite_false);
+            }
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(friendList != null)
+        {
+            return friendList.size();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public void setFriendList(List<Friend> friendList)
+    {
+        this.friendList = friendList;
+        notifyDataSetChanged();
+    }
+
+    class FriendHolder extends RecyclerView.ViewHolder
+    {
+        private CircleImageView imgProfilePicture;
+        private TextView tvName;
+        private ImageView imgFavourite;
+
+        public FriendHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProfilePicture = itemView.findViewById(R.id.imgProfilePicture);
+            tvName = itemView.findViewById(R.id.tvName);
+            imgFavourite = itemView.findViewById(R.id.imgFavourite);
+        }
+    }
+}
