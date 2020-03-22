7fc36f0e45f7826a63a273ac750e7ca3dfc126bb
schemabuoi <charyszczakg@gmail.com>
2020-03-22 09:47:51 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/friends/view/MainActivity.java
+++ app/src/main/java/com/example/friends/view/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.friends.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.example.friends.R;
+import com.example.friends.entity.Friend;
+import com.example.friends.viewmodel.FriendViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FriendViewModel friendViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        friendViewModel = new ViewModelProvider(this).get(FriendViewModel.class);
+        friendViewModel.getAll().observe(this, new Observer<List<Friend>>() {
+            @Override
+            public void onChanged(List<Friend> friends) {
+                //update RecyclerView
+            }
+        });
     }
 
 }
