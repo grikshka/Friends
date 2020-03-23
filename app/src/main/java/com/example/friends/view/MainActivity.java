c65288c016257436446430a062855dd74cb11414
schemabuoi <charyszczakg@gmail.com>
2020-03-23 01:09:35 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/friends/view/MainActivity.java
+++ app/src/main/java/com/example/friends/view/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.friends.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.friends.R;
 import com.example.friends.entity.Friend;
 import com.example.friends.viewmodel.FriendViewModel;
+import com.google.android.material.button.MaterialButton;
 
 import java.util.List;
 
@@ -20,13 +25,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeRecyclerView();
+        initializeAddFriendButton();
+    }
+
+    private void initializeRecyclerView()
+    {
+        RecyclerView recFriends = findViewById(R.id.recFriends);
+        final FriendAdapter adapter = new FriendAdapter();
+        recFriends.setLayoutManager(new LinearLayoutManager(this));
+        recFriends.setHasFixedSize(true);
+        recFriends.setAdapter(adapter);
+
         friendViewModel = new ViewModelProvider(this).get(FriendViewModel.class);
+        friendViewModel.initialize(getApplication());
         friendViewModel.getAll().observe(this, new Observer<List<Friend>>() {
             @Override
             public void onChanged(List<Friend> friends) {
-                //update RecyclerView
+                adapter.setFriendList(friends);
             }
         });
+    }
+
+    private void initializeAddFriendButton()
+    {
+        MaterialButton addFriend = findViewById(R.id.btnAddFriend);
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAddFriendActivity();
+            }
+        });
+
+    }
+
+    private void startAddFriendActivity()
+    {
+        Intent intent = new Intent(this, AddFriendActivity.class);
+        startActivity(intent);
     }
 
 }
