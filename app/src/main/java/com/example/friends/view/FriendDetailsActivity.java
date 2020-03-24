2d06818b78531a28af1733da81d4e65a6922823f
schemabuoi <charyszczakg@gmail.com>
2020-03-24 15:01:28 +0100

    Create FriendDetailsActivity

---
+++ app/src/main/java/com/example/friends/view/FriendDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.friends.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.friends.R;
+
+public class FriendDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_details);
+    }
+}
