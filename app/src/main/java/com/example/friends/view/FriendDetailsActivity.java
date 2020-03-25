79e2c5513b4c24f1a5a5d6012e75294d336b1d29
schemabuoi <charyszczakg@gmail.com>
2020-03-25 12:36:21 +0100

    Implement action bar in FriendDetails
    Implement sending message, calling, sending email and opening website (with permissions if needed)

--- app/src/main/java/com/example/friends/view/FriendDetailsActivity.java
+++ app/src/main/java/com/example/friends/view/FriendDetailsActivity.java
@@ -1,11 +1,18 @@
 package com.example.friends.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -20,6 +27,9 @@ import java.text.SimpleDateFormat;
 public class FriendDetailsActivity extends AppCompatActivity {
 
     public final static String DATE_PATTERN = "dd/MM/yyyy";
+    public final int REQUEST_CALL_PHONE = 1;
+    public final int REQUEST_SEND_SMS = 2;
+    public final int REQUEST_INTERNET = 3;
 
     private TextView tvName;
     private ImageView imgPhone;
@@ -90,15 +100,32 @@ public class FriendDetailsActivity extends AppCompatActivity {
         if(friend.getPhone().isEmpty())
         {
             imgPhone.setImageResource(R.drawable.ic_phone_disabled);
+            imgPhone.setOnClickListener(null);
             tvPhone.setTextColor(getResources().getColor(R.color.colorDisabled));
+
             imgMessage.setImageResource(R.drawable.ic_message_disabled);
+            imgMessage.setOnClickListener(null);
             tvMessage.setTextColor(getResources().getColor(R.color.colorDisabled));
         }
         else
         {
             imgPhone.setImageResource(R.drawable.ic_phone_active);
+            imgPhone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    makeCallCheckPermission();
+                }
+            });
             tvPhone.setTextColor(getResources().getColor(R.color.colorPrimary));
+
             imgMessage.setImageResource(R.drawable.ic_message_active);
+            imgMessage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendMessageCheckPermission();
+                }
+            });
+
             tvMessage.setTextColor(getResources().getColor(R.color.colorPrimary));
         }
     }
@@ -108,11 +135,18 @@ public class FriendDetailsActivity extends AppCompatActivity {
         if(friend.getEmail().isEmpty())
         {
             imgEmail.setImageResource(R.drawable.ic_email_disabled);
+            imgEmail.setOnClickListener(null);
             tvEmail.setTextColor(getResources().getColor(R.color.colorDisabled));
         }
         else
         {
             imgEmail.setImageResource(R.drawable.ic_email_active);
+            imgEmail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendMail();
+                }
+            });
             tvEmail.setTextColor(getResources().getColor(R.color.colorPrimary));
         }
     }
@@ -122,11 +156,18 @@ public class FriendDetailsActivity extends AppCompatActivity {
         if(friend.getWebsite().isEmpty())
         {
             imgWebsite.setImageResource(R.drawable.ic_website_disabled);
+            imgWebsite.setOnClickListener(null);
             tvWebsite.setTextColor(getResources().getColor(R.color.colorDisabled));
         }
         else
         {
             imgWebsite.setImageResource(R.drawable.ic_website_active);
+            imgWebsite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openWebsiteCheckPermission();
+                }
+            });
             tvWebsite.setTextColor(getResources().getColor(R.color.colorPrimary));
         }
     }
@@ -149,6 +190,7 @@ public class FriendDetailsActivity extends AppCompatActivity {
                     R.layout.friend_details_phone,
                     linFriendDataContainer,
                     true);
+
             TextView tvPhoneData = findViewById(R.id.tvPhoneData);
             tvPhoneData.setText(friend.getPhone());
         }
@@ -167,6 +209,7 @@ public class FriendDetailsActivity extends AppCompatActivity {
                     R.layout.friend_details_email,
                     linFriendDataContainer,
                     true);
+
             TextView tvEmailData = findViewById(R.id.tvEmailData);
             tvEmailData.setText(friend.getEmail());
         }
@@ -185,6 +228,7 @@ public class FriendDetailsActivity extends AppCompatActivity {
                     R.layout.friend_details_website,
                     linFriendDataContainer,
                     true);
+
             TextView tvWebsiteData = findViewById(R.id.tvWebsiteData);
             tvWebsiteData.setText(friend.getWebsite());
         }
@@ -203,12 +247,134 @@ public class FriendDetailsActivity extends AppCompatActivity {
                     R.layout.friend_details_birthday,
                     linFriendDataContainer,
                     true);
+
             TextView tvBirthdayData = findViewById(R.id.tvBirthdayData);
             tvBirthdayData.setText(dateFormatter.format(friend.getBirthday()));
         }
         else if(friend.getBirthday() == null && !(relBirthdayDataContainer == null))
         {
             linFriendDataContainer.removeView(relBirthdayDataContainer);
+        }
+    }
+
+    private void makeCallCheckPermission()
+    {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1)
+        {
+            if(checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+            {
+                makeCall();
+            }
+            else
+            {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+            }
+        }
+    }
+
+    private void makeCall()
+    {
+        String phoneNumber = friendDetailsViewModel.getFriend().getValue().getPhone();
+        Uri uri = Uri.parse("tel:" + phoneNumber);
+        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(intent);
+    }
+
+    private void sendMessageCheckPermission()
+    {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1)
+        {
+            if(checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED)
+            {
+                sendMessage();
+            }
+            else
+            {
+                requestPermissions(new String[]{Manifest.permission.SEND_SMS}, REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+    private void sendMessage()
+    {
+        String phoneNumber = friendDetailsViewModel.getFriend().getValue().getPhone();
+        Uri uri = Uri.parse("smsto:" + phoneNumber);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        startActivity(intent);
+    }
+
+    private void sendMail()
+    {
+        String emailAddress = friendDetailsViewModel.getFriend().getValue().getEmail();
+        Uri uri = Uri.parse("mailto:" + emailAddress);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        startActivity(intent);
+    }
+
+    private void openWebsiteCheckPermission()
+    {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1)
+        {
+            if(checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED)
+            {
+                openWebsite();
+            }
+            else
+            {
+                requestPermissions(new String[]{Manifest.permission.INTERNET}, REQUEST_INTERNET);
+            }
+        }
+    }
+
+    private void openWebsite()
+    {
+        String websiteAddress = friendDetailsViewModel.getFriend().getValue().getWebsite();
+        if(websiteAddress.startsWith("www"))
+        {
+            websiteAddress = websiteAddress.substring(3);
+            websiteAddress = "http://" + websiteAddress;
+        }
+        else if (!websiteAddress.startsWith("https://") && !websiteAddress.startsWith("http://")){
+            websiteAddress = "http://" + websiteAddress;
+        }
+        Uri uri = Uri.parse(websiteAddress);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        switch(requestCode)
+        {
+            case REQUEST_CALL_PHONE:
+            {
+                if(grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    makeCall();
+                }
+                break;
+            }
+            case REQUEST_SEND_SMS:
+            {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    sendMessage();
+                }
+                break;
+            }
+            case REQUEST_INTERNET:
+            {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    openWebsite();
+                }
+                break;
+            }
+
         }
     }
 }
