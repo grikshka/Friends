19573f0ae15e32509e6713e8af3a2980a2725b04
schemabuoi <charyszczakg@gmail.com>
2020-04-03 13:34:48 +0200

    Create EditFriendActivity

---
+++ app/src/main/java/com/example/friends/view/util/EditFriendActivity.java
@@ -0,0 +1,14 @@
+package com.example.friends.view.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_friend);
+    }
+}
