c941431a6a779ed7c3470094762a6b84a04ca142
schemabuoi <charyszczakg@gmail.com>
2020-04-03 16:22:41 +0200

    Create EditFriendViewModel.java

---
+++ app/src/main/java/com/example/friends/viewmodel/EditFriendViewModel.java
@@ -0,0 +1,27 @@
+package com.example.friends.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.friends.entity.Friend;
+import com.example.friends.model.FriendRepository;
+
+public class EditFriendViewModel extends ViewModel {
+
+    private FriendRepository repository;
+
+    public void initialize(Application application)
+    {
+        if(repository == null)
+        {
+            repository = new FriendRepository(application);
+        }
+    }
+
+    public void updateFriend(Friend friend)
+    {
+        repository.update(friend);
+    }
+}
